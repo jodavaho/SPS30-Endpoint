4812eb17ca464de3e5c15ffb11b1810918fcf93d
Josh Vander Hook <hook@jpl.nasa.gov>
2021-10-18 23:35:14 -0700

    Creating debs OK

---
+++ create_deb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+version=$(git tag | grep v | head -n 1)
+prefix=/usr
+
+for arch in arm-linux-gnueabi x86_64-linux-gnu
+do
+  echo "Building $version for $arch"
+  BUILDDIR=piaq-$version\_$arch
+  mkdir -p deb_build/$BUILDDIR/DEBIAN
+  cp deb/control-$arch deb_build/$BUILDDIR/DEBIAN/control
+  mkdir -p deb_build/$BULIDDIR$prefix/bin
+  mkdir -p deb_build/$BUILDDIR$prefix/share/man/man1
+  make install ARCH=$arch prefix=$prefix DESTDIR=deb_build/$BUILDDIR
+  pushd deb_build
+  dpkg-deb --build --root-owner-group $BUILDDIR
+  popd
+done
+
+mv deb_build/*.deb deb/
+rm -rf deb_build
--- create_debs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-version="1.0"
-# Exit on error
-set -e
-git checkout $version
-rmdir fakeroot -rf
-
