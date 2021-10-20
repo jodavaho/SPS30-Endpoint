0bf0d00c7f990a3da55a489dc01cbaa68aaeaceb
Josh Vander Hook <hook@jpl.nasa.gov>
2021-10-19 17:44:59 -0700

    v0.9.1 working on rasp pi 0w

--- create_deb.sh
+++ create_deb.sh
@@ -12,21 +12,38 @@ for arch in arm-linux-gnueabi x86_64-linux-gnu
 do
   echo "Building $version for $arch"
   BUILDDIR=piaq-$version\_$arch
+
+  # Make debian control files
   mkdir -p deb_build/$BUILDDIR/DEBIAN
-  cp deb/control-$arch deb_build/$BUILDDIR/DEBIAN/control
+  cp deb-control/control-$arch deb_build/$BUILDDIR/DEBIAN/control
+  cp deb-control/pre* deb_build/$BUILDDIR/DEBIAN/
+  chmod +x deb_build/$BUILDDIR/DEBIAN/pre*
+  cp deb-control/post* deb_build/$BUILDDIR/DEBIAN/
+  chmod +x deb_build/$BUILDDIR/DEBIAN/post*
+
+  # Make fake root file system
   mkdir -p deb_build/$BUILDDIR$prefix/bin
   mkdir -p deb_build/$BUILDDIR$prefix/share/man/man1
   mkdir -p deb_build/$BUILDDIR/etc/piaq
   mkdir -p deb_build/$BUILDDIR/etc/systemd/system
+
+  # Build and install files to fake root
   make install ARCH=$arch prefix=$prefix DESTDIR=deb_build/$BUILDDIR
+
+  # Touch up a few things on the docs, which were installed by above command
   sed -e "s/piaq 0.0/piaq $version/g" -i deb_build/$BUILDDIR$prefix/share/man/man1/piaq.1
   sed -e "s/Version: 0.0/Version: $version/g" -i deb_build/$BUILDDIR/DEBIAN/control
   sed -e "s/Version: v/Version: /g" -i deb_build/$BUILDDIR/DEBIAN/control
+
+  # Echo them for fun
   cat deb_build/$BUILDDIR$prefix/share/man/man1/piaq.1
   cat deb_build/$BUILDDIR/DEBIAN/control
   pushd deb_build
+
+  # Package it up
   dpkg-deb --build --root-owner-group $BUILDDIR
   popd
+
 done
 
 mv deb_build/*.deb deb/
